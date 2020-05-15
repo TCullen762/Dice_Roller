41dedfaf25803f8a5f8ab6567b69d62a21ed3e0b
Timothy Cullen <cullen762@Timothys-MacBook-Air.local>
2020-05-15 05:20:19 -0400

    began working on js, math.random function and innerHTML

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log( 'it works dummy')
+/*
+
+TODO :
+Create a variable, named dieRolls, and define it as an empty array 
+(which later you will use as a list of each die that has been rolled). (1 point)
+Add a click event listener for your "Roll!" button. (1 point)
+On click, you need to get the value from the text input box, so that you know how many dice to roll. 
+Assign this to a variable within your event listener. (1 point)
+Then you need to roll the dice. It is time to write a loop. 
+Use Math.random() to simulate rolling a single six-sided die X number of times,
+ where X is the number from the text input box. X is also the number of times you will loop. (4 points)
+Push the result of each roll onto your dieRolls array. (2 points)
+Sum all your dice rolls and display the result in the Total area on the page using innerHTML. (2 point)
+
+*/
+
+//let dieRolls =[]
+let rollResult = 0
+
+const rollInputBox= document.querySelector("#input_box")//selects HMTL node
+const rollButton = document.querySelector("#roll_button")
+rollButton.addEventListener("click",function(){
+   // console.log('funciton works')//test
+     if( Math.floor( Math.random() * 6) + 1){//the 6 represents number of dice sides
+         rollInputBox.innerHTML= rollResult += 1 //returns nothing but no errors
+         //console.log('does the button work?')//yes it works
+}
+}
+)
